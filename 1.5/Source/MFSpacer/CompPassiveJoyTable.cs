af34ddbd91d3de73d008746ba977c1462eb765ed
SokyranTheDragon <exploder1531@gmail.com>
2025-05-23 17:59:23 +0200

    Update interactive table code/XML
    - The related code was changed from a building to a comp
    - Optimizations, and improvements
    - Sight is required for gaining recreation
    - The joy type is now based off of building's XML
    - The joy gain rate is now based off of building's XML
    - Updated XML

---
+++ 1.5/Source/MFSpacer/CompPassiveJoyTable.cs
@@ -0,0 +1,46 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MFSpacer
+{
+    public class CompPassiveJoyTable : ThingComp
+    {
+        private const int JoyTickInterval = 10;
+
+        private CompPowerTrader powerTrader;
+
+        public override void CompTick()
+        {
+            if (parent.IsHashIntervalTick(JoyTickInterval) && (powerTrader == null || powerTrader.PowerOn))
+                HandlePawnJoy();
+        }
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            powerTrader = parent.GetComp<CompPowerTrader>();
+        }
+
+        public void HandlePawnJoy()
+        {
+            if (parent.def.size.x == 1 && parent.def.size.z == 1)
+            {
+                // Using this method for a 1x1 building is faster than GenAdjFast.AdjacentCellsCardinal
+                for (var index = 0; index < 4; index++)
+                    TryGivePawnJoy((parent.Position + GenAdj.CardinalDirections[index]).GetFirstPawn(parent.Map));
+            }
+            else
+            {
+                // This method is about as fast (or even very slightly faster) than caching offsets for a 2x2 building
+                var cells = GenAdjFast.AdjacentCellsCardinal(parent);
+                for (var index = 0; index < cells.Count; index++)
+                    TryGivePawnJoy(cells[index].GetFirstPawn(parent.Map));
+            }
+        }
+
+        private void TryGivePawnJoy(Pawn pawn)
+        {
+            if (pawn != null && pawn.health.capacities.CapableOf(PawnCapacityDefOf.Sight))
+                pawn.needs.joy?.GainJoy(parent.GetStatValue(StatDefOf.JoyGainFactor) * (JoyTickInterval * JoyTunings.BaseJoyGainPerHour / GenDate.TicksPerHour), parent.def.building.joyKind);
+        }
+    }
+}
