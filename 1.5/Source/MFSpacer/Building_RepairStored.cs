62f200d1ceadd6a9a332181a68464c9cc1077760
SokyranTheDragon <exploder1531@gmail.com>
2025-05-25 01:25:36 +0200

    Slightly update repair shelf C#

--- 1.5/Source/MFSpacer/Building_RepairStored.cs
+++ 1.5/Source/MFSpacer/Building_RepairStored.cs
@@ -8,64 +8,52 @@ namespace MFSpacer
     [StaticConstructorOnStartup]
     public class Building_RepairStored : Building_Storage
     {
-        private int TicksCounted = 0;
         private CompPowerTrader powerComp;
         private bool shouldAutoForbid = true;
         private Texture2D cachedCommandTex;
 
-        private Texture2D CommandTex
-        {
-            get
-            {
-                if (cachedCommandTex == null)
-                    cachedCommandTex = ContentFinder<Texture2D>.Get("Icons/Forbidden");
-                return cachedCommandTex;
-            }
-        }
+        private Texture2D CommandTex => cachedCommandTex ??= ContentFinder<Texture2D>.Get("Icons/Forbidden");
 
         public override void ExposeData()
         {
             base.ExposeData();
+
             Scribe_Values.Look(ref shouldAutoForbid, "shouldAutoForbid", true);
         }
 
-        public override void PostMapInit()
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
         {
-            base.PostMapInit();
-            powerComp = this.TryGetComp<CompPowerTrader>();
+            base.SpawnSetup(map, respawningAfterLoad);
+
+            powerComp = GetComp<CompPowerTrader>();
         }
 
         public override void Tick()
         {
             if (powerComp == null || powerComp.PowerOn)
             {
-                ++TicksCounted;
-                if (TicksCounted == 2500)
+                if (this.IsHashIntervalTick(2500))
                 {
-                    Thing firstItem = Position.GetFirstItem(Map);
-                    if (firstItem != null
-                        && firstItem.HitPoints != firstItem.MaxHitPoints
-                        && (firstItem.def.IsWithinCategory(ThingCategoryDefOf.Weapons) || firstItem.def.IsWithinCategory(ThingCategoryDefOf.Apparel)))
+                    foreach (var thing in GetSlotGroup().HeldThings)
                     {
-                        ++firstItem.HitPoints;
-                    }
+                        if (thing.HitPoints < thing.MaxHitPoints && GetParentStoreSettings().AllowedToAccept(thing))
+                            thing.HitPoints++;
 
-                    if (shouldAutoForbid && firstItem != null)
-                    {
-                        if (firstItem.HitPoints < firstItem.MaxHitPoints)
-                        {
-                            firstItem.SetForbidden(true);
-                        }
-                        else
-                        {
-                            firstItem.SetForbidden(false);
-                        }
+                        if (shouldAutoForbid)
+                            thing.SetForbidden(thing.HitPoints < thing.MaxHitPoints);
                     }
-
-                    TicksCounted = 0;
                 }
             }
+
             base.Tick();
+        }
+
+        public override void Notify_ReceivedThing(Thing newItem)
+        {
+            base.Notify_ReceivedThing(newItem);
+
+            if (shouldAutoForbid)
+                newItem.SetForbidden(newItem.HitPoints < newItem.MaxHitPoints);
         }
 
         public override IEnumerable<Gizmo> GetGizmos()
@@ -75,14 +63,15 @@ namespace MFSpacer
 
             if (Faction == Faction.OfPlayer)
             {
-                Command_Toggle commandToggle = new Command_Toggle();
-                commandToggle.hotKey = KeyBindingDefOf.Command_TogglePower;
-                commandToggle.icon = CommandTex;
-                commandToggle.defaultLabel = "EnableAutoForbid".Translate();
-                commandToggle.defaultDesc = "EnableAutoForbidExplanation".Translate();
-                commandToggle.isActive = () => shouldAutoForbid;
-                commandToggle.toggleAction = () => shouldAutoForbid = !shouldAutoForbid;
-                yield return commandToggle;
+                yield return new Command_Toggle
+                {
+                    hotKey = KeyBindingDefOf.Command_TogglePower,
+                    icon = CommandTex,
+                    defaultLabel = "EnableAutoForbid".Translate(),
+                    defaultDesc = "EnableAutoForbidExplanation".Translate(),
+                    isActive = () => shouldAutoForbid,
+                    toggleAction = () => shouldAutoForbid = !shouldAutoForbid
+                };
             }
         }
     }
