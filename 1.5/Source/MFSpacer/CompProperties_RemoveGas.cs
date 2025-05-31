4c8d2589a082720d749aebbbf0553b02aa9001b2
SokyranTheDragon <exploder1531@gmail.com>
2025-05-31 16:29:02 +0200

    Make air purifier remove gas in its cell

---
+++ 1.5/Source/MFSpacer/CompProperties_RemoveGas.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace MFSpacer;
+
+public class CompProperties_RemoveGas : CompProperties
+{
+    public List<GasType> gasTypesToRemove = null;
+    public uint Mask { get; protected set; } = uint.MaxValue;
+
+    public CompProperties_RemoveGas() => compClass = typeof(CompRemoveGas);
+
+    public override void ResolveReferences(ThingDef parentDef)
+    {
+        base.ResolveReferences(parentDef);
+
+        if (Mask == uint.MaxValue && !gasTypesToRemove.NullOrEmpty())
+        {
+            var mask = 0u;
+
+            // Set all the bytes corresponding to a specific gas to 1's
+            for (var i = 0; i < gasTypesToRemove.Count; i++)
+                mask |= (uint)byte.MaxValue << (short)gasTypesToRemove[i];
+
+            // Invert the mask, since we'll want to set those specific bits to 0 to clear the gas
+            Mask = ~mask;
+        }
+    }
+
+    public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
+    {
+        foreach (var error in base.ConfigErrors(parentDef))
+            yield return error;
+
+        if (gasTypesToRemove.NullOrEmpty())
+            yield return $"{gasTypesToRemove} has no elements, making this comp not do anything";
+        else
+        {
+            var distinct = gasTypesToRemove.Distinct().ToList();
+            if (distinct.Count != gasTypesToRemove.Count)
+            {
+                gasTypesToRemove = distinct;
+                distinct.TrimExcess();
+                yield return $"{gasTypesToRemove} cannot be distinct";
+            }
+        }
+    }
+}
---
+++ 1.5/Source/MFSpacer/CompRemoveGas.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace MFSpacer;
+
+public class CompRemoveGas : ThingComp
+{
+    protected static readonly AccessTools.FieldRef<GasGrid, uint[]> GasDensityField = AccessTools.FieldRefAccess<uint[]>(typeof(GasGrid), "gasDensity");
+
+    private CompPowerTrader powerTrader;
+
+    public CompProperties_RemoveGas Props => (CompProperties_RemoveGas)props;
+
+    public override void PostSpawnSetup(bool respawningAfterLoad)
+    {
+        powerTrader = parent.GetComp<CompPowerTrader>();
+    }
+
+    public override void CompTick()
+    {
+        if (parent.Spawned && (powerTrader == null || powerTrader.PowerOn))
+        {
+            var gasGrid = GasDensityField(parent.Map.gasGrid);
+            foreach (var pos in parent.OccupiedRect())
+            {
+                if (gasGrid[CellIndicesUtility.CellToIndex(parent.Position, parent.Map.Size.x)] > 0u)
+                {
+                    gasGrid[CellIndicesUtility.CellToIndex(parent.Position, parent.Map.Size.x)] &= Props.Mask;
+                    parent.Map.mapDrawer.MapMeshDirty(pos, MapMeshFlagDefOf.Gas);
+                }
+            }
+        }
+    }
+}
